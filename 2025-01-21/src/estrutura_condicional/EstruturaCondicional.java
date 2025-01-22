5c4b8a6c5d3e79d5c8220a7befeae3f437ff4dff
caiomvital <caiomvital@gmail.com>
2025-01-22 20:51:05 -0300

    first commit

---
+++ 2025-01-21/src/estrutura_condicional/EstruturaCondicional.java
@@ -0,0 +1,83 @@
+package estrutura_condicional;
+
+public class EstruturaCondicional {
+	public static void main(String[] args) {
+
+		int opcao = 9;
+		
+		if(opcao == 1) {
+			System.out.println("Opção escolhida: 1");
+		} else 
+		if(opcao == 2) {
+			System.out.println("Opção escolhida: 2");
+		} else 
+		if(opcao == 3) {
+			System.out.println("Opção escolhida: 3");
+		} else
+		if(opcao == 4) {
+			System.out.println("Opção escolhida: 4");
+		} else
+		if(opcao == 5) {
+			System.out.println("Opção escolhida: 5");
+		} else
+		if(opcao == 6) {
+			System.out.println("Opção escolhida: 6");
+		} else 
+		if(opcao == 7) {
+			System.out.println("Opção escolhida: 7");
+		} else 
+		if(opcao == 8) {
+			System.out.println("Opção escolhida: 8");
+		} else {
+			System.out.println("Opção inválida");
+		}
+		
+		
+		
+		
+		
+		
+		
+//		double nota = 4;
+//
+//		if (nota >= 7) {
+//			
+//			if (nota == 10) {
+//				System.out.println("Aprovado com louvor!");
+//				System.out.println("Aluno recebeu um prêmio");
+//			} else {
+//				System.out.print("Aprovado!");
+//			}
+//		} else if(nota > 4 && nota < 7) {
+//			System.out.println("Em recuperação");
+//		} else {
+//			System.out.println("Reprovado");
+//		}
+
+//		double salario = 1900;
+//		boolean aprovado = false;
+//		
+//		if(salario * 0.3 >= 600) {
+//			aprovado = true;
+//		}
+//		
+//		if(aprovado == true) {
+//			System.out.println("Parabéns");
+//		} else {
+//			System.out.println("Tente em outro momento");
+//		}
+
+		// int valor = 10;
+		// resto da divisão por 2
+		// se o resto da divisão por 2 for igual a 0
+		// o valor é par
+		// senão, o valor é ímpar
+		// ex 8 % 2 = 0 -> 8 é par
+//		if(valor % 2 == 0) {
+//			System.out.println("É par");
+//		} else {
+//			System.out.println("É ímpar");
+//		}
+
+	}
+}
