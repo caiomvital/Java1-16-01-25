19d81359a2e2e1f7e95e58767ca56f0f580ea4da
Caio de Matos Vital <81044580+caiomvital@users.noreply.github.com>
2025-02-21 10:47:55 -0300

    Projeto do dia 20/02/25 adicionado

---
+++ 2025-02-20/src/excecoes/Calculadora.java
@@ -0,0 +1,13 @@
+package excecoes;
+
+public class Calculadora {
+
+	
+	public static void dividir(double a, double b) {
+		System.out.println(a / b);
+	}
+	
+	public static double dividir(int a, int b) {
+		return a / b;
+	}
+}
